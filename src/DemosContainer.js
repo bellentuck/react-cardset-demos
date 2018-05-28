947453d54f0262369df42560e0321b4d79dfe2dc
Ben Ellentuck <ellentuckben@gmail.com>
2018-05-28 07:19:54 -0700

    first commit, establishing demos to work from to build exciting ui components

---
+++ src/DemosContainer.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const styles = {
+  display: 'flex',
+  flexDirection: 'column',
+  width: '20%',
+  margin: '10px',
+  padding: '10px',
+  height: '300px',
+  boxShadow: '3px 0px 3px 0px #ccc'
+}
+
+
+export default () => (
+  <div
+    className="DemosContainer"
+    style={styles}
+  >
+    <h1>helloworld</h1>
+  </div>
+);
