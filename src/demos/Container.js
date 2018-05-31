cec7b71db3cef709d50d6b6a56f8a667b4767d87
Ben Ellentuck <ellentuckben@gmail.com>
2018-05-31 14:16:08 -0700

    adding and amending styles for hover and click effects

--- src/demos/Container.js
+++ src/demos/Container.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import { StyleSheet, css } from 'aphrodite';
+import { StyleSheet, css } from 'aphrodite/no-important';
 
 // box shadows library:
 const shadows = {  // shades, spectres, fates......familiars
   classic: 'rgb(204, 204, 204) 0px 1px 3px 0px',
-  pressable: 'rgb(204, 204, 204) 0px -7px 3px 0px',
-  pressed: 'rgb(204, 204, 204) 0px -7px 3px -1px',
+  classicPressed: 'rgb(204, 204, 204) 0px 0px 3px 0px',
+
+  elegant: 'rgb(204, 204, 204) 1px 0px 3px 0px',
+  elegantHover: 'rgb(204, 204, 204) 1px 0px 4px 0px',
+  elegantActive: 'rgb(204, 204, 204) 1px 0px 2px 0px',
+
+  keyboard: 'rgb(204, 204, 204) 0px -7px 3px 0px',
+  keyboardPressed: 'rgb(204, 204, 204) 0px -7px 3px -1px',
 }
 
 
@@ -58,14 +64,14 @@ const styles = StyleSheet.create({
     justifyContent: 'space-around',
     width: '50%',
     height: '200px',
-    boxShadow: shadows.pressable,
+    boxShadow: shadows.elegant,
     paddingTop: '14px',
     marginLeft: '5%',
     ':hover': {
-      boxShadow: shadows.pressed,
+      boxShadow: shadows.elegantHover,
     },
     ':active': {
-      boxShadow: shadows.pressable,
+      boxShadow: shadows.elegantActive,
     }
   },
 
